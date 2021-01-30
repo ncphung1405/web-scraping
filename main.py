512b46adb06bb723d692c9045530533987e91b7d
Administrator <nguyenchinhhung140502@gmail.com>
2021-01-30 10:38:49 +0700

    tải lên

---
+++ main.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+def main():
+    url_goc = input('Nhập link khởi đầu: ')
+    url_goc = quet_url.sua_url_goc(url_goc)
+    url_tim_duoc = quet_url.tim_url_lien_quan(url_goc, url_goc)
+    waiting_line = url_tim_duoc
+    history = quet_url.them_va_duyet_hang_cho(waiting_line, url_goc)
+
+    luu_url.tao_thu_muc('bong_da_2')
+    luu_url.luu_tat_ca_file(history)
+
+
+if __name__ == '__main__':
+    main()
